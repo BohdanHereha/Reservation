3d56fecb66164079d1e3b074c46f17081ac7ba49
Bohdan Hereha <bogdangerega19@gmail.com>
2019-12-05 13:20:03 +0200

    Add new commit

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ app.post('/messages', (req, res) => {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(__dirname + '/public/'));
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
